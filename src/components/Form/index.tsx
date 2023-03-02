edb7b082cd67c2a324ef909343c27119b6a58f13
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-03-02 17:56:20 +0545

    chore: basic project layout with bootstrap

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Form = () => {
+  return (
+    <div className='px-2'>
+      <Button as='div'>Click</Button>
+    </div>
+  );
+};
+
+export default Form;
